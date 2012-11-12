f5d36b54693c2b47ae3e6af3c77ffe8a3cad87d7
Mårten Sjöstrand <marten.sjostrand@gmail.com>
2012-11-12 10:27:01 +0100

    Adding more logging to MessageParser

--- src/main/scala/com/netlight/fnnl/postalservice/MessageParser.scala
+++ src/main/scala/com/netlight/fnnl/postalservice/MessageParser.scala
@@ -2,18 +2,20 @@ package com.netlight.fnnl.postalservice
 import org.slf4j.LoggerFactory
 
 sealed case class ParserResult
-case class Sucessful(action: String, json:String) extends ParserResult
+case class Sucessful(action: String, json: String) extends ParserResult
 case class Unsucessful() extends ParserResult
 
 object MessageParser {
   val MessagePattern = """^([a-zA-Z].*):\s+(\{.*\})\r?\n?$""".r
-  val log = LoggerFactory.getLogger("com.netlight.fnnl.postalservice.MessageParser"); 
+  val log = LoggerFactory.getLogger("com.netlight.fnnl.postalservice.MessageParser");
   def parse(data: String): ParserResult = {
-	  data match {
-	    case MessagePattern(action, json) => Sucessful(action, json)
-	    case _ => {
-	      log.warn("'{}' does not match reg exp '{}'", data, MessagePattern.toString())
-	      Unsucessful()}
-	  }
+    log.debug("About to parse message: '{}'", data);
+    data match {
+      case MessagePattern(action, json) => Sucessful(action, json)
+      case _ => {
+        log.warn("'{}' does not match reg exp '{}'", data, MessagePattern.toString())
+        Unsucessful()
+      }
+    }
   }
 }
